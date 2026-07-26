cddd0786e28a6cffaf8a8d5d97d95b4caeba6f95
Senzcr <45140237+senzcr@users.noreply.github.com>
2026-07-26 12:43:22 +0000

    Added rows and columns

--- web_components/table/components/Table.js
+++ web_components/table/components/Table.js
@@ -8,19 +8,40 @@ export class Table extends HTMLElement {
     connectedCallback() {
         const headers = this.dataset.headers.split(",").map(header => header.trim());
         
-        console.log(headers);
-        
         this.shadowRoot.innerHTML = `
             <link rel="stylesheet" href="/components/Table.css" />
             <table>
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Username</th>
-                        <th>Country</th>
+                        ${headers.map(header => `<th>${header}</th>`).join("")}
                     </tr>
                 </thead>
+                <tbody></tbody>
             </table>
         `;
+    }
+    /**
+     * @param data { string[][] } 
+     */
+     
+    set data(data) {
+        const tableBody = this.shadowRoot.querySelector("tbody");
+        const rows = data.map(rowData => {
+           const row = document.createElement("tr");
+           const cells = rowData.map(cellData => {
+               const cell = document.createElement("td");
+               
+               cell.textContent = cellData;
+               
+               return cell;
+           });
+           
+           row.append(...cells);
+           
+           return row;
+        });
+        
+        tableBody.innerHTML = "";
+        tableBody.append(...rows);
     }
 }
--- web_components/table/index.js
+++ web_components/table/index.js
@@ -1,3 +1,12 @@
 import { Table } from "./components/Table.js";
 
 customElements.define("styled-table", Table);
+
+const newData = [
+  ["8831", "dcode", "Australia"],
+  ["6605", "red.square", "United States"],
+];
+
+const usersTable = document.getElementById("users");
+
+usersTable.data = newData;
